b61e8d204185a137e4c24ed1685585d241cafba0
Carlos <carlosestupina@gmail.com>
2024-07-02 15:26:39 +0200

    ejercicio 3 react

--- reactProyect/src/components/AlertClock.jsx
+++
@@ -1,9 +0,0 @@
-import React, { useState, useEffect } from 'react';
-
-function AlertClock({onClick}) {
-    
-    return  <button onClick={onClick} className='buton'>
-    Mostrar Hora
-</button>;
-}
-export default AlertClock;
