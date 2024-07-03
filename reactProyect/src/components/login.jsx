41c4f5f8043e8a678e142cb2c40465f52654a818
Carlos <carlosestupina@gmail.com>
2024-07-03 18:19:36 +0200

    ejercicio react 7

--- reactProyect/src/components/login.jsx
+++ reactProyect/src/components/login.jsx
@@ -25,27 +25,30 @@ function Login() {
             [name]: type === "checkbox" ? checked : value,
           }));
     };
-    const onLogin = (event) => {
-        event.preventDefault(); 
-        setSubmittedData(formData);
-      };
       const reset = (event) => {
         event.preventDefault(); 
         setSubmittedData(defaultData);
         setFormData(defaultData);
       }
-    return <div>
+      const HandleLogin = (event) => {
+        event.preventDefault(); 
+        setSubmittedData(formData);
+      }
+    return <form onSubmit={HandleLogin}>
     <h1>Login!</h1>
     <input type="text" id="userName" placeholder="userName" name="userName" onChange={actualizarInfo} value={formData.userName}></input>
     <input type="password" id="password" placeholder="password" name="password" onChange={actualizarInfo} value={formData.password}></input>
     <p>remember me<input type="checkbox" id="checkbox" name="remember" onChange={actualizarInfo} checked={formData.remember}></input></p>
     {formData.userName !== "" && formData.password !== "" ? (
-        <button onClick={onLogin}>login here</button>
+        <button type="submit">submit</button>
     ) : null}
     <button id="reset" onClick={reset}>Clear Form</button>
     {submittedData.userName !== "" && submittedData.password !== "" && (
         <LoginData Data={submittedData} />
       )}
-    </div>
+      
+    </form>
 }
 export default Login;
+
+//para evitar el comportamiento por defecto del formulario, y que no se recargue la pagina al hacer submit, se usa "event.preventDefault()" esto garantizara que el form no haga nada inesperado.
