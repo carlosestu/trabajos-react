b0f2b729131e6b15a2401e0e5b71bdfc015b0a83
Carlos <carlosestupina@gmail.com>
2024-07-11 12:43:22 +0200

    ejercicio 16 react

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,14 @@
 import { useState} from 'react'
 import './App.css'
-import Color from './components/Color'
-import Colors from './components/Colors'
+import {GithubUser} from './components/GithubUser'
+import FormUser from './components/FormUser'
+
 function App() {
-  const array = [
-    { id: 1, name: 'Red' },
-    { id: 2, name: 'Green' },
-    { id: 3, name: 'Blue' },
-    { id: 4, name: 'orange'}
-  ];
+
 
   return (
     <>
-    <Colors colors={array} />
+  <FormUser />
     </>
   )
 }
