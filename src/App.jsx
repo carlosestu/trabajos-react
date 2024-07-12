0f3bb103045a2b62bc732608d3414253bc5be774
Carlos <carlosestupina@gmail.com>
2024-07-12 19:15:20 +0200

    ejercicio react 18

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,20 @@
-import { useState} from 'react'
-import './App.css'
-import {CurrentLocation} from './components/CurrentLocation'
-import UseCurrentLocation from './components/UseCurrentLocation'
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import FormUser from './components/FormUser';
+import ShowGithubUser from './components/ShowGithubUser';
+import './App.css';
 
 function App() {
-
-
   return (
-    <>
-  <CurrentLocation />
-    </>
-  )
+    <div>
+      <Router>
+        <Routes>
+          <Route exact path="/" element={<FormUser />} />
+          <Route path="/users/:userName" element={<ShowGithubUser />} />
+        </Routes>
+      </Router>
+    </div>
+  );
 }
 
-export default App
+export default App;
